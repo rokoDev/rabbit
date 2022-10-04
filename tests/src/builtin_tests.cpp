8012412f51c8971d714f050f3387b2530096ab13
rokoDev <mishaDoter@gmail.com>
2022-10-04 14:03:27 +0300

    RAB-34 added test for serialization structure with member of std::vector type

--- tests/src/builtin_tests.cpp
+++ tests/src/builtin_tests.cpp
@@ -2,6 +2,7 @@
 #include <gtest/gtest.h>
 #include <user_literals/user_literals.h>
 
+#include <cstring>
 #include <vector>
 
 #include "serialization_tests.h"
@@ -44,6 +45,23 @@ inline bool operator==(const Tricky& lhs, const Tricky& rhs)
            (utils::bit_cast<uint32_t>(lhs.c) ==
             utils::bit_cast<uint32_t>(rhs.c)) &&
            (lhs.f == rhs.f) && (lhs.d == rhs.d) && (lhs.e == rhs.e);
+}
+
+struct WithVector
+{
+    int32_t a;
+    std::vector<int8_t> b;
+    float c;
+    bool d;
+};
+
+inline bool operator==(const WithVector& lhs, const WithVector& rhs)
+{
+    return (lhs.a == rhs.a) && (lhs.b.size() == rhs.b.size()) &&
+           (!std::memcmp(lhs.b.data(), rhs.b.data(), lhs.b.size())) &&
+           (utils::bit_cast<uint32_t>(lhs.c) ==
+            utils::bit_cast<uint32_t>(rhs.c)) &&
+           (lhs.d == rhs.d);
 }
 
 TEST_F(BuiltinTests, TrickySerialization)
@@ -124,4 +142,70 @@ TEST_F(BuiltinTests, EmptyVectorSerialization)
     ASSERT_EQ(readerPos, bit_pos(1));
     ASSERT_EQ(vec.size(), restored.size());
     ASSERT_EQ(vec.size(), 0_uz);
+}
+
+TEST_F(BuiltinTests, VectorOfChars)
+{
+    using VecT = std::vector<char>;
+    constexpr std::size_t kSize = 100_uz;
+    VecT vec;
+    vec.reserve(kSize);
+
+    char value = static_cast<char>(-50);
+    for (std::size_t i = 0_uz; i < kSize; ++i)
+    {
+        vec.push_back(value++);
+    }
+
+    VecT restored{};
+    bit_pos readerPos{};
+    bit_pos writerPos{};
+    execute(
+        [&]() -> result<void>
+        {
+            BOOST_LEAF_AUTO(w, rabbit::make_bin_writer(rawBuf_));
+            BOOST_LEAF_AUTO(r, rabbit::make_bin_reader(rawBuf_));
+            BOOST_LEAF_CHECK(rabbit::serialize(w, vec));
+            BOOST_LEAF_ASSIGN(restored, rabbit::deserialize<VecT>(r));
+            readerPos = r.pos();
+            writerPos = w.pos();
+            return {};
+        });
+
+    ASSERT_EQ(readerPos, writerPos);
+    ASSERT_EQ(readerPos, bit_pos((kSize + sizeof(uint32_t)) * CHAR_BIT));
+    ASSERT_EQ(vec.size(), restored.size());
+    ASSERT_EQ(vec.size(), kSize);
+    for (std::size_t i = 0_uz; i < kSize; ++i)
+    {
+        ASSERT_EQ(restored[i], vec[i]);
+    }
+}
+
+TEST_F(BuiltinTests, StructWithVector)
+{
+    const WithVector toSave{1111_i32, {-120, 20, 5}, 100.234f, true};
+    WithVector restored{};
+    bit_pos readerPos{};
+    bit_pos writerPos{};
+    execute(
+        [&]() -> result<void>
+        {
+            BOOST_LEAF_AUTO(w, rabbit::make_bin_writer(rawBuf_));
+            BOOST_LEAF_AUTO(r, rabbit::make_bin_reader(rawBuf_));
+            BOOST_LEAF_CHECK(rabbit::serialize(w, toSave));
+            BOOST_LEAF_ASSIGN(restored, rabbit::deserialize<WithVector>(r));
+            readerPos = r.pos();
+            writerPos = w.pos();
+            return {};
+        });
+    ASSERT_EQ(readerPos, writerPos);
+    const bit_pos expectedBitPos(
+        (sizeof(int32_t) + sizeof(uint32_t) +
+         sizeof(decltype(toSave.b)::value_type) * toSave.b.size() +
+         sizeof(float)) *
+            CHAR_BIT +
+        1);
+    ASSERT_EQ(readerPos, expectedBitPos);
+    ASSERT_EQ(restored, toSave);
 }
